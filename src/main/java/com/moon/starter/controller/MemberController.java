b842e40aa697c2aa85548c5fed5d7cc342a91adc
eunjin <jinneey05@gmail.com>
2021-03-19 11:08:55 +0900

    회원가입폼구현

---
+++ src/main/java/com/moon/starter/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.moon.starter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+
+	@RequestMapping("/member/join")
+	public String memberJoin() {
+		
+		return "member/join";
+	}
+
+}
