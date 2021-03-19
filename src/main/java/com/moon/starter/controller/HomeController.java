67d4cffdc23bfc1bc17199f20555920567b4f715
eunjin <jinneey05@gmail.com>
2021-03-19 17:53:44 +0900

    beforActionInterceptor 적용하여, 매번 로그인한 회워의 정보를 DB 에서 꺼내오는 작업을 간편화

--- src/main/java/com/moon/starter/controller/HomeController.java
+++ src/main/java/com/moon/starter/controller/HomeController.java
@@ -16,35 +16,37 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @Slf4j
 public class HomeController {
-	
+
 	@Autowired
 	MemberService memberService;
-	
+
 	@RequestMapping("/home/main")
-	public String showMain(HttpSession session, Model model) {
-		
-		long loginedMemberId = 0;
-		
-		if(session.getAttribute("loginedMemberId")!=null) {
-			loginedMemberId = (long)session.getAttribute("loginedMemberId");
-		}
-		
-		Member loginedMember = memberService.getOne(loginedMemberId);
-		model.addAttribute("loginedMember", loginedMember);
-		
-		log.info("loginedMember : "+loginedMember);
-		
-		
+//	public String showMain(HttpSession session, Model model) {
+//		
+//		long loginedMemberId = 0;
+//		// 로그인 했는지 체크하고 
+//		if(session.getAttribute("loginedMemberId")!=null) {
+//			loginedMemberId = (long)session.getAttribute("loginedMemberId");
+//		}
+//		//해당 로그인 회원의 번호로 DB에서 회원의 전체 정보를 불러오는 작업
+//		// 이걸 거의 모든 액션에서 해줘야한다.... -> 인터셉터 도입 
+//		//인터셉터는 매 액션 실행 전에 실행이 되어,request 객체에 필요한 정보를 주입해 준다. 
+//		
+//		Member loginedMember = memberService.getOne(loginedMemberId);
+//		model.addAttribute("loginedMember", loginedMember);
+//		
+//		log.info("loginedMember : "+loginedMember);
+//		
+//		
+//		return "home/main";
+//	}
+	public String showMain(Model model) {
 		return "home/main";
 	}
-	
+
 	@RequestMapping("/")
 	public String showMain2() {
 		return "redirect:/home/main";
 	}
-	
-	
-	
-	
-	
+
 }
