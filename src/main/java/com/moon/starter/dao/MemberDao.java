6be886b9a3b1ff5cfa2f6cefdd9e503f96e81b81
eunjin <jinneey05@gmail.com>
2021-03-19 11:46:32 +0900

    MemberService,MemberDao 추가, 회원로그인 ID 중복처리기능추가 후 회원가입에 적용

---
+++ src/main/java/com/moon/starter/dao/MemberDao.java
@@ -0,0 +1,10 @@
+package com.moon.starter.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MemberDao {
+
+	public int getLoiginIdDupCount(String loginId);
+
+}
---
+++ src/main/java/com/moon/starter/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.moon.starter.service;
+
+import java.util.Map;
+
+public interface MemberService {
+
+	public Map<String, Object> checkLoginIdDup(String loginId);
+
+	
+	
+
+}
